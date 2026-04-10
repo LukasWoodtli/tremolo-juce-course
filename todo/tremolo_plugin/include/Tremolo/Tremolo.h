ec51174373cb6f1cfe5801fe7ca35dc61e44376d
Lukas Woodtli <woodtli.lukas@gmail.com>
2026-04-10 13:46:30 +0200

    Small refactoring

--- todo/tremolo_plugin/include/Tremolo/Tremolo.h
+++ todo/tremolo_plugin/include/Tremolo/Tremolo.h
@@ -9,28 +9,31 @@ public:
       true);
   }
   void prepare(double sampleRate, int expectedMaxFramesPerBlock) {
-    const juce::dsp::ProcessSpec spec {
-      .sampleRate = sampleRate,
-      .maximumBlockSize = static_cast<juce::uint32>(expectedMaxFramesPerBlock),
-      .numChannels =  1u};
-    lfo.prepare(spec);
+    const juce::dsp::ProcessSpec processSpec{
+        .sampleRate = sampleRate,
+        .maximumBlockSize =
+            static_cast<juce::uint32>(expectedMaxFramesPerBlock),
+        .numChannels = 1u,
+    };
+    lfo.prepare(processSpec);
   }
 
   void process(juce::AudioBuffer<float>& buffer) noexcept {
     // for each frame
     for (const auto frameIndex : std::views::iota(0, buffer.getNumSamples())) {
-      const auto lfoVal = lfo.processSample(0.f);
+      const auto lfoValue = lfo.processSample(0.f);
 
-      // TODO: calculate the modulation value
+      // calculate the modulation value
+      constexpr float modulationDepth = 0.4f;
+      const auto modulationValue = modulationDepth * lfoValue + 1.f;
 
-      // for each channel sample in the frame
       for (const auto channelIndex :
            std::views::iota(0, buffer.getNumChannels())) {
         // get the input sample
         const auto inputSample = buffer.getSample(channelIndex, frameIndex);
 
-        // TODO: modulate the sample
-        const auto outputSample = 0.1f * lfoVal;
+        // modulate the sample
+        const auto outputSample = modulationValue * inputSample;
 
         // set the output sample
         buffer.setSample(channelIndex, frameIndex, outputSample);
